ab63c20a5a054f2f51b4936fbeaaf959c1bb20fd
Eric Garcia <egarcia1868@gmail.com>
2020-03-04 20:03:29 -0600

    I think this damn thing is finally working properly...

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,15 +26,29 @@ module.exports = (app) => {
     fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db, null, 2), "utf8", err => {
       if (err) throw err;
       console.log("appended!");
-      res.json(db);
+      res.json(db)
     });
     
   });
 
   // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-//   app.delete("/api/notes/:id", (req, res) => {
-//     let chosen = req.params.id;
+  app.delete("/api/notes/:id", (req, res) => {
+    let chosen = parseInt(req.params.id);
+    console.log("delete step 1 reached");
 
-//     for (let i = 0; i < db[0].length)
-//   })
+    for (let i = 0; i < db.length; i++) {
+      console.log("delete step 2 reached");
+      console.log(`chosen: ${chosen} | db[i].id: ${db[i].id}`);
+      console.log(`TYPES chosen: ${typeof chosen} | db[i].id: ${typeof db[i].id}`);
+      if (chosen === db[i].id) {
+        db.splice(i, 1);
+        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db, null, 2), "utf8", err => {
+          if (err) throw err;
+          console.log("deleted!");
+          res.json(db)
+        });
+        break;
+      }
+    }
+  })
 }
