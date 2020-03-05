380df2b53e7fa51d700e3f93e6f7603538ef8a31
Eric Garcia <egarcia1868@gmail.com>
2020-03-04 19:43:31 -0600

    HOLY SWEET JESUS...  finally got the new note to auto populate to the left

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,40 @@
-let db = require("../db/db.json");
+const fs = require("fs");
+const db = require("../db/db.json");
+const path = require("path")
 
 module.exports = (app) => {
 
-  // This should bring up the info from the db.json
+  // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
   app.get("/api/notes", (req, res) => {
     res.json(db);
   });
 
-// This "should" push the new note to the db.json.  "should", but idk
+  // * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
   app.post("/api/notes", (req, res) => {
-    db.push(req.body);
+    let newID;
+    if (db.length < 1) {
+      newID = 0;
+    } else {
+      newID = db[db.length-1].id+1;
+    };
+    let newNote = req.body;
+    newNote.id = newID;
+    // let StrNote = JSON.stringify(newNote);
+    // let strDb = JSON.stringify(db);
+    //                                 console.log(`newNote: ${StrNote} | newID: ${newID} | db: ${strDb}`);
+    db.push(newNote);
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db, null, 2), "utf8", err => {
+      if (err) throw err;
+      console.log("appended!");
+      res.json(db);
+    });
+    
   });
+
+  // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+//   app.delete("/api/notes/:id", (req, res) => {
+//     let chosen = req.params.id;
+
+//     for (let i = 0; i < db[0].length)
+//   })
 }
